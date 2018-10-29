60ddffee80014493300d3a1d3f04be18752b5fe5
LLanta <luka.lanta@gmail.com>
2018-10-29 15:34:18 +0100

    Added timer in map activity

--- app/src/main/java/com/example/luka/googlemapsandgogleplaces/ResultPreviewActivity.java
+++ app/src/main/java/com/example/luka/googlemapsandgogleplaces/ResultPreviewActivity.java
@@ -1,23 +1,121 @@
 package com.example.luka.googlemapsandgogleplaces;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.w3c.dom.Text;
 
-public class ResultPreviewActivity extends AppCompatActivity {
+public class ResultPreviewActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15;
+
+    private boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
+    RunProperties properties;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_preview);
+        getLocationPermission();
+        initMap();
         TextView tvMessage = findViewById(R.id.tvMessage);
 
         Intent intent = getIntent();
-        RunProperties properties = (RunProperties) intent.getSerializableExtra("2001");
+        properties = (RunProperties) intent.getSerializableExtra("2001");
 
         tvMessage.setText(String.valueOf(properties.points.get(0).get(1).lat));
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) ResultPreviewActivity.this);
+    }
+
+    private void getLocationPermission(){
+        String[] permissions ={Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+                initMap();
+            }
+            else {
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionsGranted=false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if (grantResults.length>0){
+                    for(int i = 0 ; i<grantResults.length;i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionsGranted=true;
+                    //initialize map
+                    initMap();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) { //On map ready enable blue dot
+        mMap = googleMap;
+        if (mLocationPermissionsGranted) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            drawRunRoute();
+            Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void drawRunRoute(){
+        Polyline line;
+
+
+        for (int i = 0; i < properties.points.size(); i++) {
+            PolylineOptions options = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
+            for(int y=0;y<properties.points.get(i).size();y++) {
+                SerializableLatLng point = properties.points.get(i).get(y);
+                options.add(point.getLatLng());
+            }
+            line = mMap.addPolyline(options);
+        }
+
     }
 }
